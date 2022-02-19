7debdb73b60303d8ff4174a2eb06f32cb1ca8cba
Spiz <71483926+spiz97@users.noreply.github.com>
2022-02-19 23:43:59 +0900

    version 2.5

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
-#modules : Activation, Optimizer, Layer, spizflow_functions, main
+#modules : Activation, Optimizer, Layer, spizflow_functions
+#main : main_test_version
 
 __all__ = ['Acitivation', 'Optimizer','Layer','spizflow_functions']
